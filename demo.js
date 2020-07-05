dbfc6796dda2cb6cbafcb98d5ccb6a68a1d516e5
mayuri789 <50314927+mayuri789@users.noreply.github.com>
2020-07-05 23:26:29 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,70 @@
+console.log('hey this is my first typeScript code!!!!');
+var a1 = 10;
+console.log('hi my value is' + a1);
+// variable declaration
+//data type any,number,string,boolean,undefined,null
+var c;
+var d;
+var a1 = 10;
+console.log("Value of A1 is " + a1);
+var m2;
+console.log("**********************");
+console.log("Value is " + m2);
+var str4;
+console.log("***************************");
+console.log("Value  Str1 is " + str4);
+console.log("**************");
+var a;
+var b;
+var result1 = function (a, b) {
+    var result = a + b;
+    return result;
+};
+console.log('addition is : ' + result1(5, 10));
+var a3;
+a3 = "mayuri mestry";
+// console.log("Value of A3 is "+a3);
+//  a3=20;
+console.log("Value of A3 is " + a3);
+console.log("Current Data type of A3 is " + typeof a3);
+a3 = 'Typescript...';
+console.log("Value of A3 is " + a3 + " data type is " + typeof (a3));
+var a4 = 20;
+//type assertion
+// variable should have any datatype
+//type assertion
+// variable should have any datatype
+//we can use type assertion in two ways
+// use as syntax
+// Angle bracket
+var a5;
+a5 = 20;
+//(<string> a5).
+//2 as syntax
+a5 = "Mayuri";
+console.log(a5.concat(" Mestry"));
+//airthematic,logical,bitwise,conditional,ternary,comparision.
+//increment,decrement,assignment
+//+,*,-,/....
+var a = 10;
+var b = 30;
+var res = a + b;
+console.log("Addition is " + res);
+var res1 = a < 9 ? "Hi" : "Bye";
+console.log("Output of a is  " + res1);
+//Loop
+//if,if..else,nestated if else 
+var e = 12;
+if (e < 10) {
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is false");
+}
+var c2 = 5;
+while (c2 != 0) {
+    console.log("Value of a is " + c2);
+    //c2=c2-1;
+    c2--;
+}
+console.log(" value of while loop... " + c2);
